6593a6c9062cf0878219537080d38448ccc3b99e
Bandla Gineesh <94602631+BGineesh@users.noreply.github.com>
2022-12-13 18:23:31 +0530

    Update SquarePattern.java

--- SquarePattern.java
+++ SquarePattern.java
@@ -1,9 +1,9 @@
-Print the following pattern for the given N number of rows.
-Pattern for N = 4
-4444
-4444
-4444
-4444
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 4
+// 4444
+// 4444
+// 4444
+// 4444
 
 
 
